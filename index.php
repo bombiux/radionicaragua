2ca2bb61a7b45b5af93bbb45b28ea61be2ace717
mecatesl <cristianrg333@gmail.com>
2020-07-08 16:26:02 -0600

    pruebas

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 <main id="content">
+<?php echo 'ola k ase' ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'entry' ); ?>
 <?php comments_template(); ?>
